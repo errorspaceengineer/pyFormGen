29a477b9e574289109ac792abcd4afdca19f5d98
Han Duong <114118476+errorspaceengineer@users.noreply.github.com>
2023-01-18 21:49:52 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.0.0',
     license='GPLv3',
     packages=find_packages(),
-    url='https://github.com/reilleya/pyFormGen',
+    url='https://github.com/errorspaceengineer/pyFormGen',
     description='A library that generates pyQt5 forms for entering data with units'
 )
